1f4709c3ad0e4de95130287e0c55ebc2e6be7907
lxien <yuanyicode@gmail.com>
2026-08-19 10:06:59 +0800

    chore: bump version to 3.1.0-beta.1

--- docs/src/components/DownloadMatrix/index.tsx
+++ docs/src/components/DownloadMatrix/index.tsx
@@ -6,7 +6,7 @@ import styles from './styles.module.css';
 
 const REPO = 'orbien-org/orbien';
 
-const FALLBACK_VERSION = '3.1.0-dev';
+const FALLBACK_VERSION = '3.1.0-beta.1';
 
 type OsId = 'windows' | 'linux' | 'darwin' | 'freebsd';
 type ArchId = 'amd64' | 'arm64';
